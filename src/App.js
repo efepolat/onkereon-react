6cc72acebc7678cc82987be0a42b0f440413ab77
efepolat <efeozpolat@hotmail.com>
2022-09-25 04:07:50 +0300

    Create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-function App() {
-  return (
-    <h1 className="text-3xl font-bold underline bg-yellow-200">
-      Hello world!
-    </h1>
-  );
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgetPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+
+function App(){
+  return(
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" elemenet={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forget-password" element={<ForgetPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App;
